0426db83910b3ffa84469866edc95a447457617c
Ilshin Ji <ilshin714@gmail.com>
2023-05-20 10:07:36 -0400

    Initial Backend Api Created

---
+++ organic-green-api/src/main/java/com/ecommerce/organicgreen/OrganicGreenApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.organicgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicGreenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganicGreenApiApplication.class, args);
+    }
+
+}
---
+++ organic-green-api/src/test/java/com/ecommerce/organicgreen/OrganicGreenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.organicgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicGreenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
