3532be50fd309ac0025bebfc44ffb45fee5ebca7
Ilshin Ji <ilshin714@gmail.com>
2023-05-21 09:43:17 -0400

    Added Navbar

---
+++ organic-green-ui/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center">
+        <h1 className="text-3xl font-bold text-white">Organic Green</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
