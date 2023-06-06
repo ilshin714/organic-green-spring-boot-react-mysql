c74a80e392a2e18a54a035ce5a06e019cc9010ca
Ilshin Ji <ilshin714@gmail.com>
2023-06-05 23:52:38 -0400

    arranged alt text

--- organic-green-ui/src/components/Item.js
+++ organic-green-ui/src/components/Item.js
@@ -19,6 +19,7 @@ export default function Item() {
           <img
             src="https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80"
             className="w-full h-full object-cover"
+            alt="product"
           />
         </CardHeader>
         <CardBody>
--- organic-green-ui/src/components/Slider.js
+++ organic-green-ui/src/components/Slider.js
@@ -21,17 +21,17 @@ export default function Slider() {
       >
         <img
           src="https://images.unsplash.com/photo-1610348725531-843dff563e2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="image 1"
+          alt="banner 1"
           className="h-full w-full object-cover"
         />
         <img
           src="https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="image 2"
+          alt="banner 2"
           className="h-full w-full object-cover"
         />
         <img
           src="https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="image 3"
+          alt="banner 3"
           className="h-full w-full object-cover"
         />
       </Carousel>
